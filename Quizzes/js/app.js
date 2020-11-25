ebcbc49e7dcd8ed1c4cc870ed3c7fb614a173c5c
Kanghyun Jo <kjo0@outlook.com>
2020-11-24 20:31:33 -0500

    Global component, data properties and child coponent Quiz 3-3

--- Quizzes/js/app.js
+++ Quizzes/js/app.js
@@ -1,24 +1,25 @@
-var tlist = {
-  template: '<p> This is another child local componet. </P>'
-};
+// 할일 #1
+// sibling-component 를 이름으로 갖는 새로운 컴포넌트를 아래에 등록해보세요.
+// options : template, props
 
-// 실습 #1 - `todo-footer` 컴포넌트 전역 등록
-// <p>This is another child global component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
 
-Vue.component('todo-footer', {
-  template: '<p>This is another child global componet</p>'
+Vue.component('sibling-component', {
+  props: ['propsdata'],
+  template: '<p>{{ propsdata }}</p>'
 });
 
+Vue.component('child-component', {
+  props: ['propsdata'],
+  template: '<p>{{ propsdata }}</p>'
+});
+
+// 할일 #2
+// data 속성을 1개 더 지정하고 (예: anotherMessage) 임의의 문자열을 값으로 대입해보세요.
+// 새로 지정한 data 속성은 위 sibling-component에 props로 전달합니다.
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'This is a parent component'
-  },
-
-  // 실습 #2 - `todo-list` 컴포넌트 지역 등록
-  // <p>This is another child local component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
-
-  components: {
-    'todo-list': tlist
+    message: 'Hello Vue! passed from Parent Component',
+    anotherMessage: 'Hey Vue! This is another message.'
   }
 });
